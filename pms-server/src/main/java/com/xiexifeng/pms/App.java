8f343cdaeb88f5bf2abae0fc1190534c048c53c0
xiezb <xiezb@bwoil.com>
2020-11-09 11:03:22 +0800

    init

---
+++ pms-server/src/main/java/com/xiexifeng/pms/App.java
@@ -0,0 +1,13 @@
+package com.xiexifeng.pms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
